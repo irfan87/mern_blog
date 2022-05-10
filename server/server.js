d73a8689003f572fd04ef15210ba26fe444a42ce
Ahmad Irfan Mohamamd Shukri <nerve2009@yahoo.com>
2022-05-11 03:17:31 +0800

    create routes and implement authentication functionality with bcrypt

--- server/server.js
+++ server/server.js
@@ -1,15 +1,20 @@
 const express = require("express");
 const dotenv = require("dotenv");
 const mongoose = require("mongoose");
+const authRoute = require("./routes/auth");
 
 const app = express();
 
 dotenv.config();
 
+app.use(express.json());
+
 mongoose
 	.connect(process.env.MONGO_URL)
 	.then(console.log("You mongoDB is running"))
 	.catch((e) => console.error(e));
+
+app.use("/api/auth", authRoute);
 
 const PORT = 5000 || process.env.PORT;
 
