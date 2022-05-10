83711e0826de5fcff3cb633f876179b11e19437e
Ahmad Irfan Mohamamd Shukri <nerve2009@yahoo.com>
2022-05-11 01:49:23 +0800

    mongoDB connection

--- server/server.js
+++ server/server.js
@@ -1,13 +1,17 @@
 const express = require("express");
+const dotenv = require("dotenv");
+const mongoose = require("mongoose");
 
 const app = express();
 
-const PORT = 5000 || process.env.PORT;
+dotenv.config();
 
-app.use("/", (req, res) => {
-	// console.log("this is main URL");
-	res.send("This is main URL");
-});
+mongoose
+	.connect(process.env.MONGO_URL)
+	.then(console.log("You mongoDB is running"))
+	.catch((e) => console.error(e));
+
+const PORT = 5000 || process.env.PORT;
 
 app.listen(PORT, (req, res) => {
 	console.log(`Blog API is running on port ${PORT}`);
